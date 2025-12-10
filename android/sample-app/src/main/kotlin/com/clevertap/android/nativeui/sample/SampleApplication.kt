5e2a139e0039ca0026ba22f8685dc4b9c1d8c0dd
CTLalit <144685420+CTLalit@users.noreply.github.com>
2025-12-10 21:13:34 +0530

    task(SDK-5405): Fixes spacing issue
    Removes code so app can be compiled.

--- android/sample-app/src/main/kotlin/com/clevertap/android/nativeui/sample/SampleApplication.kt
+++ android/sample-app/src/main/kotlin/com/clevertap/android/nativeui/sample/SampleApplication.kt
@@ -1,28 +1,7 @@
 package com.clevertap.android.nativeui.sample
 
 import android.app.Application
-import coil.ImageLoader
-import coil.ImageLoaderFactory
-import coil.disk.DiskCache
-import coil.memory.MemoryCache
 
-class SampleApplication : Application(), ImageLoaderFactory {
-    
-    override fun newImageLoader(): ImageLoader {
-        return ImageLoader.Builder(this)
-            .memoryCache {
-                MemoryCache.Builder(this)
-                    .maxSizePercent(0.25)
-                    .build()
-            }
-            .diskCache {
-                DiskCache.Builder()
-                    .directory(cacheDir.resolve("image_cache"))
-                    .maxSizeBytes(50 * 1024 * 1024)
-                    .build()
-            }
-            .crossfade(true)
-            .respectCacheHeaders(false)
-            .build()
-    }
+class SampleApplication : Application() {
+
 }
